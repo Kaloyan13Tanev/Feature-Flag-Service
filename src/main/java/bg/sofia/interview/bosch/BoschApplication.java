b0dead3cda82fc8206c3f2b9d14ed9acdb2f54f6
Kaloyan13Tanev <104224203+Kaloyan13Tanev@users.noreply.github.com>
2026-06-25 12:29:59 +0300

    Initial commit

---
+++ src/main/java/bg/sofia/interview/bosch/BoschApplication.java
@@ -0,0 +1,13 @@
+package bg.sofia.interview.bosch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoschApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoschApplication.class, args);
+	}
+
+}
