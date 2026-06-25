6fbec96f1aea892a549ff4ae1650071a19873f38
Kaloyan13Tanev <104224203+Kaloyan13Tanev@users.noreply.github.com>
2026-06-25 12:45:55 +0300

    Add FeatureFlag entity
    Create FeatureFlag entity class with parameters id, name, description, enabled, created and updated timestamps.

---
+++ src/main/java/bg/sofia/interview/bosch/model/FeatureFlag.java
@@ -0,0 +1,44 @@
+package bg.sofia.interview.bosch.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class FeatureFlag {
+
+    public FeatureFlag() {
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String name;
+    private String description;
+
+    private boolean enabled;
+
+    @CreatedDate
+    private Instant createdAt;
+    @LastModifiedDate
+    private Instant updatedAt;
+    
+}
